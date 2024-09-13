c01749f735726cf15cd56fdb2323c862cda58a7e
C.Vignesh <162482102+VIGNESH1280@users.noreply.github.com>
2024-09-13 22:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	let a = 0; // F(0)
+    let b = 1; // F(1)
+
+    // Iterate through to find the num-th Fibonacci number
+    for (let i = 2; i <= num; i++) {
+        // Compute the next Fibonacci number
+        const next = a + b;
+        // Update a and b for the next iteration
+        a = b;
+        b = next;
+    }
+
+    // Return the num-th Fibonacci number
+    return b;
+	
 }
 
 module.exports = fibonacci;
